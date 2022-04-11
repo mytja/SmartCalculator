be270989471516fbc0a0c121dd4a279a1662ba4f
mytja <mytja@protonmail.com>
2022-04-11 20:33:18 +0200

    Add fully functioning formulas

--- main.py
+++ main.py
@@ -183,35 +183,63 @@ class FormulaProviders:
     speed = FormulaProvider("Hitrost", "v", "m/s")
     distance = FormulaProvider("Pot", "s", "m")
     force = FormulaProvider("Sila", "F", "N")
-    accelaration = FormulaProvider("Pospešek", "a", "m/(s**2)")
-    kinetic_energy = FormulaProvider("Kinetična energija", "Wk", "J")
-    potential_energy = FormulaProvider("Potencialna energija", "Wp", "J")
+    accelaration = FormulaProvider("Pospesek", "a", "m/(s**2)")
+    time = FormulaProvider("Cas", "t", "s")
+    kinetic_energy = FormulaProvider("Kin. en.", "Wk", "J")
+    potential_energy = FormulaProvider("Pot. en.", "Wp", "J")
     work = FormulaProvider("Delo", "A", "J")
-    gravitational_accelaration = FormulaProvider("Gravitacijski pospešek", "g",  "m/(s**2)")
-    height = FormulaProvider("Višina", "h", "m")
-    start_speed = FormulaProvider("Začetna hitrost", "v1", "m/s")
-    end_speed = FormulaProvider("Končna hitrost", "v2", "m/s")
-	
+    gravitational_accelaration = FormulaProvider("G posp.", "g",  "m/(s**2)")
+    height = FormulaProvider("Visina", "h", "m")
+    start_speed = FormulaProvider("Zac. hitr.", "v1", "m/s")
+    end_speed = FormulaProvider("Kon. hitr.", "v2", "m/s")
+    delta_temperature = FormulaProvider("Delta temp.", "ΔT", "K")
+    specific_heat_capacity = FormulaProvider("Spec. topl.", "c", "J/(kg*K)")
+    kelvin = FormulaProvider("Kelvin", "K", "K")
+    celsius = FormulaProvider("Celzija", "°C", "°C")
+    resistance = FormulaProvider("Upor", "Ohm", "Ω")
+    current = FormulaProvider("Napetost", "U", "V")
+    voltage = FormulaProvider("Tok", "I", "A")
 
 
 class Formula:
-	def __init__(self, formula_name, formula, description, calculation_formula, providers):
-		self.formula_name = formula_name
-		self.formula = formula
-		self.description = description
-		self.calculation_formula = calculation_formula
-		self.providers = providers
+    def __init__(self, formula_name, formula, description, calculation_formula, providers):
+        self.formula_name = formula_name
+        self.formula = formula
+        self.description = description
+        self.calculation_formula = calculation_formula
+        self.providers = providers
 
 
 class Formulas:
     formulas = [
-        Formula("Delo", "A=F*s", "Izračun dela iz sile in poti", "F*s", [FormulaProviders.force, FormulaProviders.distance]),
-        Formula("Kinetična energija", "Wk=(m*(v**2))/2", "Izračun kinetične energije iz hitrosti in mase", "(m*(v**2))/2", [FormulaProviders.mass, FormulaProviders.speed]),
-        Formula("Potencialna energija iz mase", "Wp=m*g*h", "Izračun potencialne energije iz mase in višine", "m*10*h", [FormulaProviders.mass, FormulaProviders.height]),
-        Formula("Potencialna energija iz sile", "Wp=F*h", "Izračun potencialne energije iz sile in višine", "F*h", [FormulaProviders.force, FormulaProviders.height]),
-        Formula("Povprečna hitrost", "v_avg=(v1+v2)/2", "Izračun povprečne hitrosti iz začetne in končne hitrosti", "(v1+v2)/2", [FormulaProviders.start_speed, FormulaProviders.end_speed]),
+        # Formule povezane z delom
+        Formula("Delo", "A=F*s", "Izracun dela iz sile in poti", "F*s", [FormulaProviders.force, FormulaProviders.distance]),
+        # Formule povezane z kinetično energijo
+        Formula("Povprecna hitrost", "v_avg=(v1+v2)/2", "Izracun povprecne hitrosti iz zacetne in koncne hitrosti", "(v1+v2)/2", [FormulaProviders.start_speed, FormulaProviders.end_speed]),
+        Formula("Kineticna energija", "Wk=(m*(v**2))/2", "Izracun kineticne energije iz hitrosti in mase", "(m*(v**2))/2", [FormulaProviders.mass, FormulaProviders.speed]),
+        # Formule povezane z potencialno energijo
+        Formula("Potencialna energija iz mase", "Wp=m*g*h", "Izracun potencialne energije iz mase in visine", "m*10*h", [FormulaProviders.mass, FormulaProviders.height]),
+        Formula("Potencialna energija iz sile", "Wp=F*h", "Izracun potencialne energije iz sile in visine", "F*h", [FormulaProviders.force, FormulaProviders.height]),
+        # Formule povezane s toploto
+        Formula("Toplota", "Q=m*c*ΔT", "Izracun toplote iz specificne toplote, mase in temperaturne razlike", "m*ΔT*c", [FormulaProviders.specific_heat_capacity, FormulaProviders.delta_temperature, FormulaProviders.mass]),
+        Formula("Temperatura", "C=K-273", "Izracun temperature iz Kelvinov v Celzije", "K-273", [FormulaProviders.kelvin]),
+        Formula("Temperatura", "K=°C+273", "Izracun temperature iz Celzija v Kelvin", "°C+273", [FormulaProviders.celsius]),
+        
+        
+        # Formule povezane s hitrostjo
+        Formula("Hitrost", "v=sqrt((Wk*2)/m)", "Izracun hitrosti iz kineticne energije in mase", "sqrt((Wk*2)/m)", [FormulaProviders.kinetic_energy, FormulaProviders.mass]),
+        Formula("Koncna hitrost", "v2=v1+a*t", "Velja samo pri enakomernem pospesenem gibanju", "v1+a*t", [FormulaProviders.start_speed, FormulaProviders.time, FormulaProviders.accelaration]),
+        # Formule povezane s potjo
+        Formula("Pot", "s=(a*(t**2))/2", "Izracun poti iz pospeska in casu", "(a*(t**2))/2", [FormulaProviders.accelaration, FormulaProviders.time]),
+        Formula("Pot", "s=((v1+v2)/2)*t", "Izracun poti iz zacetne in koncne hitrosti ter casa", "((v1+v2)/2)*t", [FormulaProviders.start_speed, FormulaProviders.time, FormulaProviders.end_speed]),
+        
+        
+        # Formule povezane z elektriko in električnim tokom
+        Formula("Upor", "R=U/I", "Izracun upora iz napetosti in toka", "U/I", [FormulaProviders.current, FormulaProviders.voltage]),
+        Formula("Tok", "I=U/R", "Izracun toka iz napetosti in upora", "U/R", [FormulaProviders.resistance, FormulaProviders.voltage]),
+        Formula("Napetost", "U=I*R", "Izracun napetosti iz toka in upora", "I*R", [FormulaProviders.resistance, FormulaProviders.current]),
     ]
-    
+
     @staticmethod
     def formula_preparation(provider_state):
         formula = Formulas.formulas[provider_state.current_formula]
@@ -220,17 +248,20 @@ class Formulas:
         return formula.calculation_formula
 
     @staticmethod
-    def draw_formula_overview(current_formula: int):
+    def lcd_formula_overview(current_formula):
         f = Formulas.formulas[current_formula]
 
         lcd.fill(0)
         lcd.text(f.formula_name, 0, 0)
         for i in range(1, 4):
-            lcd.text(f.description, 0, i*8)
+            lcd.text(f.description[(i-1)*16:i*16], 0, i*8)
         lcd.text(f.formula, 0, 32)
         for i in range(5, 8):
-            provider = f.providers[i-5]
-            lcd.text(f"{provider.provider_name} {provider.provider_formula_name}/{provider.unit}")
+            try:
+                provider = f.providers[i-5]
+                lcd.text(f"{provider.provider_name} {provider.provider_formula_name}/{provider.unit}", 0, i*8)
+            except:
+                pass
         lcd.show()
 
 
@@ -303,7 +334,8 @@ while True:
         elif m == Buttons.menu:
             state = State.formula_overview
             current_formula = 0
-            Formulas.draw_formula_overview(current_formula)
+            Formulas.lcd_formula_overview(current_formula)
+            time.sleep(1)
         elif m == Buttons.cancel:
             state = State.calculate
             current_formula = 0
@@ -332,13 +364,13 @@ while True:
                     current_formula += 1
                 else:
                     current_formula = 0
-                Formulas.draw_formula_overview(current_formula)
+                Formulas.lcd_formula_overview(current_formula)
         elif m == Buttons.up:
             if state == State.formula_overview:
                 if current_formula < 1:
                     current_formula = 0
                 else:
                     current_formula -= 1
-                Formulas.draw_formula_overview(current_formula)
+                Formulas.lcd_formula_overview(current_formula)
     time.sleep(0.2)
 
